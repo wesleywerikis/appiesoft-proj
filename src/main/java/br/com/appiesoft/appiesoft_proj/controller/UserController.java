446ec75c63c3a4940ddef6fc858b5e06063cc9c5
Wesley Werikis <wesleywerikis@outlook.com>
2025-04-03 17:38:03 -0300

    feat: Implemented method 'login' with request 'POST' for authentication

--- src/main/java/br/com/appiesoft/appiesoft_proj/controller/UserController.java
+++ src/main/java/br/com/appiesoft/appiesoft_proj/controller/UserController.java
@@ -1,13 +1,17 @@
 package br.com.appiesoft.appiesoft_proj.controller;
 
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.appiesoft.appiesoft_proj.dto.AuthLoginDto;
 import br.com.appiesoft.appiesoft_proj.dto.UserDto;
 import br.com.appiesoft.appiesoft_proj.model.User;
+import br.com.appiesoft.appiesoft_proj.secutiry.JwtUtil;
 import br.com.appiesoft.appiesoft_proj.service.UserService;
 import lombok.RequiredArgsConstructor;
 
@@ -15,13 +19,28 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class UserController {
-    
+
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody UserDto userDto){
+    public ResponseEntity<User> register(@RequestBody UserDto userDto) {
         User newUser = userService.register(userDto);
         return ResponseEntity.ok(newUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthLoginDto authLoginDto) {
+        try {
+            User user = userService.authenticate(authLoginDto.getEmail(), authLoginDto.getPassword());
+            String token = jwtUtil.generateToken(user.getEmail());
+
+            return ResponseEntity.ok().body(Map.of(
+                    "token", token,
+                    "user", user.getName()));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(401).body(Map.of("error", e.getMessage()));
+        }
     }
 
 }
