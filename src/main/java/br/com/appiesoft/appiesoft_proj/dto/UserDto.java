0ac3295ecbe66c1b2731d51d872586f2fb76d4dd
Wesley Werikis <wesleywerikis@outlook.com>
2025-04-03 15:50:21 -0300

    feaat: Created UserDto with attribute (name, email and password) for transfer the data

---
+++ src/main/java/br/com/appiesoft/appiesoft_proj/dto/UserDto.java
@@ -0,0 +1,11 @@
+package br.com.appiesoft.appiesoft_proj.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private String name;
+    private String email;
+    private String password;
+
+}
