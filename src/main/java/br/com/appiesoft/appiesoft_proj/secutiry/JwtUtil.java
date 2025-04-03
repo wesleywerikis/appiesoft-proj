5dcffcaf363292283a6536a43c62e929dd399435
Wesley Werikis <wesleywerikis@outlook.com>
2025-04-03 17:20:30 -0300

    feat: Created JwtUtiil with methods (getSignKey, generateToken, getEmailFromToken and validateToken)

---
+++ src/main/java/br/com/appiesoft/appiesoft_proj/secutiry/JwtUtil.java
@@ -0,0 +1,46 @@
+package br.com.appiesoft.appiesoft_proj.secutiry;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+    private final String jwtSecret = "Xy7!aB9@kL3#pQ5$wT8&uV1*oZ6^rM2F";
+    private final long jwtExpirationMs = 86400000;
+
+    private Key getSignKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSignKey()).build()
+                .parseClaimsJws(token)
+                .getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
