21ddc0b31cf49fe78b2d22c4531141ca3713ba7c
Wesley Werikis <wesleywerikis@outlook.com>
2025-04-03 15:51:33 -0300

    feat: Created UserService with method 'register' for register new user

---
+++ src/main/java/br/com/appiesoft/appiesoft_proj/service/UserService.java
@@ -0,0 +1,32 @@
+package br.com.appiesoft.appiesoft_proj.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import br.com.appiesoft.appiesoft_proj.dto.UserDto;
+import br.com.appiesoft.appiesoft_proj.model.User;
+import br.com.appiesoft.appiesoft_proj.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User register(UserDto userDto){
+        if(userRepository.findByEmail(userDto.getEmail()).isPresent()){
+            throw new RuntimeException("Email já cadastrado");
+        }
+
+        User user = new User();
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+
+}
