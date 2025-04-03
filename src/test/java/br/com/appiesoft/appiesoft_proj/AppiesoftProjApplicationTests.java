44529b357e36e77cfba0e50ad8d7ce4dbd9f1ebe
Wesley Werikis <wesleywerikis@outlook.com>
2025-04-03 16:00:13 -0300

    feat: AppiesoftProjApplication test

---
+++ src/test/java/br/com/appiesoft/appiesoft_proj/AppiesoftProjApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.appiesoft.appiesoft_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppiesoftProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
