10e81f78f90b3f2447c4730f67a1167c6afde611
Wesley Werikis <wesleywerikis@outlook.com>
2025-04-03 15:59:41 -0300

    feat: AppiesoftProjApplication main

---
+++ src/main/java/br/com/appiesoft/appiesoft_proj/AppiesoftProjApplication.java
@@ -0,0 +1,13 @@
+package br.com.appiesoft.appiesoft_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppiesoftProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppiesoftProjApplication.class, args);
+	}
+
+}
