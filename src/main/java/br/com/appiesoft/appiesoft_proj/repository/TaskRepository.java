c966a4a5c058afd8b38190f12e3a48dcf67240ef
Wesley Werikis <wesleywerikis@outlook.com>
2025-04-10 17:43:21 -0300

    feat: Add methods to TaskRepository for filtering and pagination

--- src/main/java/br/com/appiesoft/appiesoft_proj/repository/TaskRepository.java
+++ src/main/java/br/com/appiesoft/appiesoft_proj/repository/TaskRepository.java
@@ -2,12 +2,21 @@ package br.com.appiesoft.appiesoft_proj.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.appiesoft.appiesoft_proj.model.Priority;
 import br.com.appiesoft.appiesoft_proj.model.Task;
 import br.com.appiesoft.appiesoft_proj.model.User;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     List<Task> findByUser(User user);
+
+    List<Task> findByUserAndCompleted(User user, Boolean completed);
+
+    List<Task> findByUserAndPriority(User user, Priority priority);
+
+    Page<Task> findByUser(User user, Pageable pageable);
 }
