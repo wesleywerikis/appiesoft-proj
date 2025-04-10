8941836482b5e04d8c26005f6a2c00dd3eee9e63
Wesley Werikis <wesleywerikis@outlook.com>
2025-04-10 17:40:58 -0300

    feat: Add validation annotations to UserDto fields

--- src/main/java/br/com/appiesoft/appiesoft_proj/dto/UserDto.java
+++ src/main/java/br/com/appiesoft/appiesoft_proj/dto/UserDto.java
@@ -1,11 +1,21 @@
 package br.com.appiesoft.appiesoft_proj.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserDto {
+
+    @NotBlank(message = "Nome é obrigatório")
     private String name;
+
+    @Email(message = "E-mail inválido")
+    @NotBlank(message = "E-mail é obrigatório")
     private String email;
+
+    @Size(min = 4, message = "A senha deve ter pelo menos 4 caracteres")
     private String password;
 
 }
