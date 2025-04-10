b7c57c55c77d4b70c598f1be6a8c9d3d1a4fff54
Wesley Werikis <wesleywerikis@outlook.com>
2025-04-10 17:20:14 -0300

    feat: Add validation annotations to TaskDto fields

--- src/main/java/br/com/appiesoft/appiesoft_proj/dto/TaskDto.java
+++ src/main/java/br/com/appiesoft/appiesoft_proj/dto/TaskDto.java
@@ -3,14 +3,22 @@ package br.com.appiesoft.appiesoft_proj.dto;
 import java.time.LocalDate;
 
 import br.com.appiesoft.appiesoft_proj.model.Priority;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class TaskDto {
     
+    @NotBlank(message = "O título é obrigatório")
     private String title;
+
     private String description;
+    
+    @NotNull(message = "A data de vencimento é obrigatória")
     private LocalDate dueDate;
+
+    @NotNull(message = "A prioridade é obrigatória")
     private Priority priority;
 
 }
