b0930d1bcc4533273eef1476dfa0bad95555ece9
Wesley Werikis <wesleywerikis@outlook.com>
2025-04-10 14:37:53 -0300

    refactor: Rename User entity columns to match database naming conventions

--- src/main/java/br/com/appiesoft/appiesoft_proj/model/User.java
+++ src/main/java/br/com/appiesoft/appiesoft_proj/model/User.java
@@ -19,15 +19,16 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "user_name", nullable = false)
     private String name;
 
-    @Column(unique = true, nullable = false)
+    @Column(name = "user_email", unique = true, nullable = false)
     private String email;
 
-    @Column(nullable = false)
+    @Column(name = "user_password", nullable = false)
     private String password;
 
 }
