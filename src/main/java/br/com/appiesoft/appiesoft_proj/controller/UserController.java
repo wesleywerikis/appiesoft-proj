82dbbb860b26ebe73f4e972452c41e50614e49f9
Wesley Werikis <wesleywerikis@outlook.com>
2025-04-10 09:43:07 -0300

    feat: add '/me' endpoint to return authenticated user info

--- src/main/java/br/com/appiesoft/appiesoft_proj/controller/UserController.java
+++ src/main/java/br/com/appiesoft/appiesoft_proj/controller/UserController.java
@@ -3,6 +3,8 @@ package br.com.appiesoft.appiesoft_proj.controller;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.appiesoft.appiesoft_proj.dto.AuthLoginDto;
 import br.com.appiesoft.appiesoft_proj.dto.UserDto;
 import br.com.appiesoft.appiesoft_proj.model.User;
-import br.com.appiesoft.appiesoft_proj.secutiry.JwtUtil;
+import br.com.appiesoft.appiesoft_proj.security.JwtUtil;
+import br.com.appiesoft.appiesoft_proj.security.UserDetailsImpl;
 import br.com.appiesoft.appiesoft_proj.service.UserService;
 import lombok.RequiredArgsConstructor;
 
@@ -41,6 +44,21 @@ public class UserController {
         } catch (RuntimeException e) {
             return ResponseEntity.status(401).body(Map.of("error", e.getMessage()));
         }
+    }
+
+    @PostMapping("/me")
+    public ResponseEntity<?> getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.getPrincipal() instanceof UserDetailsImpl userDetailsImpl) {
+            User user = userDetailsImpl.getUser();
+
+            return ResponseEntity.ok(Map.of(
+                    "id", user.getId(),
+                    "name", user.getName(),
+                    "email", user.getEmail()));
+        }
+
+        return ResponseEntity.status(401).body(Map.of("error", "Usuário não autenticado"));
     }
 
 }
